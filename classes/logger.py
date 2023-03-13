51c23bb7ed6fff1a3b005c965d532dfc340d713d
EranYermiyahu <82023584+EranYermiyahu@users.noreply.github.com>
2023-03-13 11:43:35 +0200

    logger per sim

---
+++ classes/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+class logger:
+	def __init__(self, logger_path):
+		self.logger_obj = logging.getLogger(__name__)
+		self.logger_obj.setLevel(logging.INFO)
+		file_handler = logging.FileHandler(logger_path)
+		formatter = logging.Formatter('%(asctime)s: %(levelname)s - %(message)s')
+		file_handler.setFormatter(formatter)
+		# Add the file handler to the logger
+		self.logger_obj.addHandler(file_handler)
+
+	def info(self, messege):
+		# Log a message to the file
+		self.logger_obj.info('This is a log message')
